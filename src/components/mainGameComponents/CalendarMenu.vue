ddb4c1700d55a5f5b5399a40f9f8a8acbc6b8cf1
Benedikt345 <92359939+Benedikt345@users.noreply.github.com>
2022-10-23 20:20:19 +0200

    add full calendar

--- src/components/mainGameComponents/CalendarMenu.vue
+++ src/components/mainGameComponents/CalendarMenu.vue
@@ -1,8 +1,23 @@
 <template>
   <div>
-
+    <FullCalendar :options="calendarOptions"/>
   </div>
 </template>
+
+<script setup>
+import FullCalendar from '@fullcalendar/vue3'
+import DayGridPlugin from '@fullcalendar/daygrid'
+import TimeGridPlugin from '@fullcalendar/timegrid'
+import InteractionPlugin from '@fullcalendar/interaction'
+import ListPlugin from '@fullcalendar/list'
+import {reactive} from "vue";
+
+const calendarOptions = reactive({
+  plugins: [DayGridPlugin, TimeGridPlugin, InteractionPlugin, ListPlugin],
+  initialView: 'dayGridMonth',
+  initialDate: '2023-01-01',
+});
+</script>
 
 <script>
 export default {
