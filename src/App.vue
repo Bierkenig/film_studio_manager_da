1805530e36fce7c60497fb0ba352c4f335d6493c
MUC00 <91524150+MUC00@users.noreply.github.com>
2022-10-25 16:17:34 +0200

    app updated

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,36 @@
 <template>
   <div>
+    <game-header
+        v-show="this.showOnPage.includes(this.$route.name)"
+        :studioname="this.$store.getters.getStudio.name"
+        :budget="this.$store.getters.getBalance"
+        :page-name="this.$route.name"/>
+
+    <router-view/>
+
+    <menu-nav
+        v-show="this.showOnPage.includes(this.$route.name)"
+        page-name="calendar"
+    />
+
+    <audio id="backgroundMusic" autoPlay loop>
+      <source src="./backgroundMusic/backgroundMusic.mp3" type="audio/mpeg">
+    </audio>
   </div>
 </template>
 
 <script>
+import MenuNav from "@/components/mainGameComponents/MenuNav";
+import GameHeader from "@/components/mainGameComponents/GameHeader";
+
 export default {
   name: 'App',
-  components: {
-  },
+  components: {GameHeader, MenuNav},
+  data() {
+    return {
+      showOnPage: ['home', 'news', 'movies', 'library', 'streaming', 'finances', 'calendar']
+    }
+  }
 }
 </script>
 
