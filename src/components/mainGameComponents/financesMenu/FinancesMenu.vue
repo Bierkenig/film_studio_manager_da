09e781acf77335a57cfc3828d447dcf1975dd822
MUC00 <91524150+MUC00@users.noreply.github.com>
2022-10-31 18:20:36 +0100

    added first 2 sections to finances

--- src/components/mainGameComponents/financesMenu/FinancesMenu.vue
+++ src/components/mainGameComponents/financesMenu/FinancesMenu.vue
@@ -1,6 +1,24 @@
 <template>
   <div>
+    <div id="credit">
+      <h3>{{$t('labelTakeCredit')}}</h3>
+      <label for="credit">{{$t('descCredit')}}</label>
+      <button name="credit">
+        {{$t('takeCredit')}}
+      </button>
+    </div>
 
+    <div id="buy">
+      <h3>{{$t('labelBuyStudio')}}</h3>
+      <label for="buy">{{$t('descStudio')}}</label>
+      <button name="buy">
+        {{$t('buyStudio')}}
+      </button>
+    </div>
+
+    <div>
+      
+    </div>
   </div>
 </template>
 
