f10ba4c4dd3232b783e8e8c10399de7e7ccc24de
Bierkenig <92359399+Bierkenig@users.noreply.github.com>
2022-11-11 19:07:27 +0100

    added icons

--- src/components/kitchenSink/KitchenSink.vue
+++ src/components/kitchenSink/KitchenSink.vue
@@ -4,24 +4,20 @@
       <button id="startMenuButton" class="buttonStyle">Menu</button>
     </router-link>
     <div class="kitchenSinkComponentDiv">
-      <code-view code='<background-tile title="Title">Test</background-tile>' />
+      <code-view code='<background-tile title="Title">Test</background-tile>'/>
       <background-tile title="Title">Test</background-tile>
     </div>
     <div class="kitchenSinkComponentDiv">
-      <code-view code='<custom-button theme="light" size="medium">Test</custom-button>' />
-      <custom-button theme="light" size="medium">Test</custom-button>
-    </div>
-    <div class="kitchenSinkComponentDiv">
-      <code-view code='<custom-button theme="dark" size="medium">Test</custom-button>' />
-      <custom-button theme="dark" size="medium">Test</custom-button>
-    </div>
-    <div class="kitchenSinkComponentDiv">
-      <code-view code='<custom-button theme="light" size="large">Test</custom-button>' />
-      <custom-button theme="light" size="large">Test</custom-button>
-    </div>
-    <div class="kitchenSinkComponentDiv">
-      <code-view code='<custom-button theme="dark" size="large">Test</custom-button>' />
-      <custom-button theme="dark" size="large">Test</custom-button>
+      <code-view code='<custom-button theme="light" size="medium">Test</custom-button>'/>
+      <code-view code='<custom-button theme="dark" size="medium">Test</custom-button>'/>
+      <code-view code='<custom-button theme="light" size="large">Test</custom-button>'/>
+      <code-view code='<custom-button theme="dark" size="large">Test</custom-button>'/>
+      <div id="kitchenSinkCustomButtons">
+        <custom-button theme="light" size="medium">Test</custom-button>
+        <custom-button theme="dark" size="medium">Test</custom-button>
+        <custom-button theme="light" size="large">Test</custom-button>
+        <custom-button theme="dark" size="large">Test</custom-button>
+      </div>
     </div>
   </div>
 </template>
@@ -30,6 +26,7 @@
 import BackgroundTile from "@/components/kitchenSink/BackgroundTile";
 import CustomButton from "@/components/kitchenSink/CustomButton";
 import CodeView from "@/components/kitchenSink/CodeView";
+
 export default {
   name: "KitchenSink",
   components: {CodeView, CustomButton, BackgroundTile},
@@ -39,5 +36,9 @@ export default {
 <style scoped>
 .kitchenSinkComponentDiv {
   margin: 1em 1em 0 1em;
+}
+#kitchenSinkCustomButtons {
+  display: flex;
+  flex-direction: row;
 }
 </style>
