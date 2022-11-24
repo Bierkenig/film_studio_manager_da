aefa90d9ce5a7e1d15ed8433938c4d08d0f910a4
MUC00 <91524150+MUC00@users.noreply.github.com>
2022-11-24 10:42:55 +0100

    removed temp

--- src/components/mainGameComponents/preProduction/durationSection.vue
+++ src/components/mainGameComponents/preProduction/durationSection.vue
@@ -1,14 +1,6 @@
 <template>
   <div>
-    <div>{{$t('durationSection.set')}}</div>
-    <div v-for="(el, index) in screenplay" :key="index">
-      {{$t('durationSection.length')}}: {{el.length}} / {{$t('durationSection.scope')}}: {{el.details.scope}} / {{$t('durationSection.castSize')}}:
-      {{$t('durationSection.main')}}: {{el.roles.main.length}}, {{$t('durationSection.support')}}: {{el.roles.support.length}},
-      {{$t('durationSection.minor')}}: {{el.roles.minor.length}}, {{$t('durationSection.cameo')}}: {{el.roles.cameo.length}}, {{$t('durationSection.voice')}}: {{el.roles.voiceOver.length}}
-    </div>
-    <!-- swiper with production length-->
-    <input type="range" min="" max="" step="">
-    <div></div>
+
   </div>
 </template>
 
@@ -18,7 +10,6 @@ export default {
 
   data() {
     return {
-      screenplay: this.$store.getters.getCurrentScreenplay
     }
   },
 }
