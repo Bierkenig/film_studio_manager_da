8c35349cda97c7366d62adbaf737b1b6d2c4e20f
Bierkenig <92359399+Bierkenig@users.noreply.github.com>
2023-01-03 21:39:18 +0100

    updated background tile

--- src/components/kitchenSink/BackgroundTile.vue
+++ src/components/kitchenSink/BackgroundTile.vue
@@ -59,15 +59,17 @@ export default {
 <style scoped>
 .backgroundTileMainDiv {
   display: inline-block;
-  background-image: linear-gradient(var(--fsm-dark-blue-2), var(--fsm-dark-blue-3));
   border-radius: var(--fsm-l-border-radius);
   padding: var(--fsm-l-border-radius);
-  box-shadow: 0 0 5px 5px var(--fsm-dark-shadow-color);
   width: 100%;
   box-sizing: border-box;
   font-weight: var(--fsm-fw-regular);
   font-size: 22px;
   color: v-bind('colorValue');
+  background: rgba(37, 45, 62, 0.66);
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(20px);
+  -webkit-backdrop-filter: blur(20px);
 }
 
 .backgroundTileIcon {
