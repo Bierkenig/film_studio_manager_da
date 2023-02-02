3cea3b77680fca8b5b1e7030f83e54f5b1048674
Bierkenig <92359399+Bierkenig@users.noreply.github.com>
2023-02-02 11:28:21 +0100

    updated own service section

--- src/components/mainGameComponents/streamingMenu/OwnServiceSection.vue
+++ src/components/mainGameComponents/streamingMenu/OwnServiceSection.vue
@@ -1,39 +1,37 @@
 <template>
-  <div id="ownServiceSection">
-    <h2 id="ownServiceSectionHeader">Information</h2>
-    <div id="ownServiceSectionInfoContainer">
-      <streaming-info-element
-          :streaming-info-title="streamingService._name"
-          :icon="this.$store.getters.getCurrentLogo"
-          :popularity="streamingService._popularity"
-          :subscribers="roundBudget(this.streamingService._subscribers)"
-          :revenue="this.streamingService._profit.toString()"
-          :initial-price="this.streamingService._price"/>
-    </div>
-  </div>
+  <background-tile id="ownServiceSection" title="Information">
+    <streaming-info-element
+        :streaming-info-title="streamingService._name"
+        :icon="this.$store.getters.getCurrentLogo"
+        :popularity="streamingService._popularity"
+        :subscribers="roundBudget(this.streamingService._subscribers)"
+        :revenue="this.streamingService._profit.toString()"
+        :initial-price="this.streamingService._price"/>
+  </background-tile>
 </template>
 
 <script>
 import StreamingInfoElement from "@/components/kitchenSink/StreamingInfoElement.vue";
+import BackgroundTile from "@/components/kitchenSink/BackgroundTile.vue";
 
 export default {
   name: "OwnServiceSection",
-  components: {StreamingInfoElement},
+  components: {BackgroundTile, StreamingInfoElement},
 
-  data(){
+  data() {
     return {
       streamingService: this.$store.getters.getOwnStreamingService,
     }
   },
 
   watch: {
-    'this.$store.getters.getOwnStreamingService._price': function (){
+    'this.$store.getters.getOwnStreamingService._price': function () {
       console.log(this.$store.getters.getOwnStreamingService._price)
     }
   },
 
   methods: {
-    roundBudget(labelValue){
+    roundBudget(labelValue) {
       return Math.abs(Number(labelValue)) >= 1.0e+9
 
           ? (Math.abs(Number(labelValue)) / 1.0e+9).toFixed(2) + " B"
@@ -53,95 +51,4 @@ export default {
 </script>
 
 <style scoped>
-#ownServiceSection {
-  display: flex;
-  justify-content: center;
-  background-color: var(--fsm-dark-blue-3);
-  border-radius: var(--fsm-l-border-radius);
-  padding: 15px;
-}
-
-#ownServiceSectionHeader {
-  margin: 0 !important;
-  font-weight: var(--fsm-fw-bold);
-  color: var(--fsm-pink-1);
-}
-
-#ownServiceSectionInfoContainer {
-  margin-top: 15px;
-}
-
-#ownServiceSectionNameContainer {
-  display: flex;
-  flex-direction: row;
-  gap: 25px;
-  align-items: center;
-  margin-bottom: 10px;
-}
-
-#ownServiceSectionName {
-  font-weight: var(--fsm-fw-bold);
-  color: var(--fsm-white);
-  font-size: 20px;
-}
-
-#ownServiceSectionInformation {
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  margin-right: 15px;
-}
-
-.ownServiceSectionInfoElement {
-  width: 100%;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-  padding: 8px;
-  border-radius: var(--fsm-s-border-radius);
-  background-color: var(--fsm-dark-blue-3);
-}
-
-.ownServiceSectionInfoElementLeftSide {
-  display: flex;
-  flex-direction: row;
-  gap: 10px;
-  align-items: center;
-}
-
-.ownServiceSectionRangeSlider {
-  width: 130px;
-  height: 10px;
-}
-
-.ownServiceSectionRangeSlider::-webkit-slider-thumb {
-  background: linear-gradient(to right, var(--fsm-pink-1) 0%, var(--fsm-pink-1) 50%, var(--fsm-dark-blue-4) 50%, var(--fsm-dark-blue-4) 100%);
-}
-
-span {
-  width: 80%;
-  text-align: center;
-  font-weight: var(--fsm-fw-bold);
-  cursor: pointer;
-}
-
-span.num {
-  pointer-events: none;
-}
-
-.ownServiceSectionPriceButton {
-  font-weight:var(--fsm-fw-bold);
-  border-radius: 50%;
-  background: var(--fsm-pink-1);
-  border-style: none;
-}
-
-#minusButton {
-  margin-right: 10px;
-}
-
-#plusButton {
-  margin-left: 10px;
-}
 </style>
