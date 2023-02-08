d78611d7b88f78171ecf56ad6f3e62b6946657d0
MUC00 <91524150+MUC00@users.noreply.github.com>
2023-02-08 12:29:22 +0100

    finished build Options & pre + Production Summaries

--- src/components/mainGameComponents/currentProduction/productionSummary.vue
+++ src/components/mainGameComponents/currentProduction/productionSummary.vue
@@ -1,13 +1,102 @@
 <template>
-  <div></div>
+  <div>
+    <div class="modal-mask">
+      <div class="modal-wrapper" @click="$emit('close')">
+        <div class="modal-container" @click.stop="">
+          <div class="modal-body">
+            <slot name="body">
+              <h2>{{$t('summaries.production.title')}}</h2>
+              <div>{{$t('summaries.production.progress')}}</div>
+              <progress :max="maxWeeks" :value="currentWeeks">
+              </progress>
+              <div>{{percent}}%</div>
+              <div>{{$t('summaries.production.events')}}</div>
+              <div v-for="(el, index) in happenedEvents" :key="index">
+                {{$t('productionEvents.' + el + ".problem")}}
+              </div>
+            </slot>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "productionSummary"
+  name: "productionSummary",
+
+  data() {
+    return {
+      happenedEvents: [],
+      maxWeeks: 0,
+      currentWeeks: 0,
+      percent: 0,
+    }
+  },
+
+  mounted() {
+    this.happenedEvents = this.$store.getters.getCurrentMovie._production.happenedEvents
+    this.maxWeeks = this.$store.getters.getCurrentMovie._preProduction.preProductionLength +
+        this.$store.getters.getCurrentMovie._preProduction.productionLength +
+        this.$store.getters.getCurrentMovie._preProduction.postProductionLength
+    this.currentWeeks = this.$store.getters.getCurrentMovie._preProduction.productionLength
+
+    this.percent = Math.round((this.currentWeeks * 100) / this.maxWeeks)
+  }
 }
 </script>
 
 <style scoped>
+.modal-mask {
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: table;
+  transition: opacity 0.3s ease;
+}
 
+.modal-wrapper {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+.modal-container {
+  width: 300px;
+  margin: 0px auto;
+  padding: 5px 30px 20px 30px;
+  background-color: var(--fsm-dark-blue-3);
+  border-radius: var(--fsm-l-border-radius);
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+  transition: all 0.3s ease;
+  font-family: Helvetica, Arial, sans-serif;
+}
+
+.modal-body {
+  margin: 20px 0;
+  text-align: center;
+}
+
+/*
+ * The following styles are auto-applied to elements with
+ * transition="modal" when their visibility is toggled
+ * by Vue.js.
+ *
+ * You can easily play with the modal transition by editing
+ * these styles.
+ */
+
+.modal-enter-from, .modal-leave-to {
+  opacity: 0;
+}
+
+.modal-enter-active .modal-container,
+.modal-leave-active .modal-container {
+  -webkit-transform: scale(1.1);
+  transform: scale(1.1);
+}
 </style>
--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,28 @@ module.exports = {
     pluginOptions: {
         electronBuilder: {
             preload: 'src/preload.js',
+            builderOptions: {
+                "appId": "com.fsm.app",
+                "productName": "Film Studio Manager",
+                "target": "NSIS",
+                "nsis": {
+                    "allowToChangeInstallationDirectory": true,
+                    "oneClick": false
+                },
+            },
+            // Use this to change the entrypoint of your app's main process
+            mainProcessFile: 'src/background.js',
+            // Use this to change the entry point of your app's render process. default src/[main|index].[js|ts]
+            rendererProcessFile: 'src/main.js',
+            // Provide an array of files that, when changed, will recompile the main process and restart Electron
+            // Your main process file will be added by default
+            //mainProcessWatch: ['src/myFile1', 'src/myFile2'],
+            // Provide a list of arguments that Electron will be launched with during "electron:serve",
+            // which can be accessed from the main process (src/background.js).
+            // Note that it is ignored when --debug flag is used with "electron:serve", as you must launch Electron yourself
+            // Command line args (excluding --debug, --dashboard, and --headless) are passed to Electron as well
+            //mainProcessArgs: ['--arg-name', 'arg-value']
+            outputDir: 'build'
         }
     }
 }
