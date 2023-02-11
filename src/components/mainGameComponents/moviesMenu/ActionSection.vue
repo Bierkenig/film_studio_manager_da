50dcbb9ce3b0ff16dd62347b2b8c0e18db94c069
Benedikt345 <8119@htl.rennweg.at>
2023-02-11 10:54:45 +0100

    set award events and started with movie generation

--- src/components/mainGameComponents/moviesMenu/ActionSection.vue
+++ src/components/mainGameComponents/moviesMenu/ActionSection.vue
@@ -28,7 +28,7 @@ export default {
   methods: {
     goToSite(){
       if(this.headline === 'createMovie'){
-        this.$store.state.currentMovie = new Movie(this.$store.state.studio, null)
+        this.$store.state.currentMovie = new Movie(this.$store.state.studio, 0)
         this.$store.getters.getCurrentMovie._foundationDate = this.$store.getters.getCurrentDate;
         this.$router.push({name: 'screenplaySection'});
       } else if(this.headline === 'listOfSources'){
