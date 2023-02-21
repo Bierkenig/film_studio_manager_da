6fba486d19a98aea9063272e638b6c5483ca5f15
Benedikt345 <92359939+Benedikt345@users.noreply.github.com>
2023-02-22 00:14:35 +0100

    fixed sound effect bug

--- src/services/store.js
+++ src/services/store.js
@@ -30,7 +30,7 @@ export default createStore({
         currentPostProdEventType: "",
         currentScreenplay: null,
         logo: null,
-        soundeffects: false,
+        soundeffects: true,
         backgroundMusic: true,
         currentDate: new Date("2023-01-01T00:00:00.000Z"),
         currentLanguage: 'en',
