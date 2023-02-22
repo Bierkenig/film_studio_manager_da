c65e914ca46247bf7d6423226bd1201e272f496a
Bierkenig <92359399+Bierkenig@users.noreply.github.com>
2023-02-22 21:46:23 +0100

    Updated Person Edit

--- src/components/DB-Editor/Entities/StudioEdit.vue
+++ src/components/DB-Editor/Entities/StudioEdit.vue
@@ -1,27 +1,40 @@
 <template>
-  <div>
-    <label for="name">Studio Name</label>
-    <input id="name" v-model="name">
+  <div id="studioEditMainDiv">
+    <icon-button
+        id="dbEditorBackButton"
+        icon="simple-arrow-left"
+        size="medium"
+        :dark="true"
+        :bg-gradient="true"
+        :icon-gradient="false"
+        :shadow="false"
+        @click="$router.go(-1)"
+    />
+    <background-tile title="Edit Studio" id="studioEditBgTile">
+      <div class="studioEditInputLine">
+        <label for="name">Studio Name</label>
+        <input id="name" v-model="name">
+      </div>
+      <div class="studioEditInputLine">
+        <label for="popularity">Studio Popularity</label>
+        <input id="popularity" type="Number"
+               @change="() => { if(popularity > 100 || popularity < 1) { this.popularity = 0 }}" v-model="popularity">
+      </div>
+      <custom-button id="saveButton" class="buttonStyle" @click="writeDB(studio)">Save</custom-button>
+    </background-tile>
   </div>
-
-  <br/>
-
-  <div>
-    <label for="popularity">Studio Popularity</label>
-    <input id="popularity" type="Number" @change="() => { if(popularity > 100 || popularity < 1) { this.popularity = 0 }}" v-model="popularity">
-  </div>
-
-  <button id="backButton" class="buttonStyle" @click="this.$router.go(-1)">Back</button>
-  <button id="saveButton" class="buttonStyle" @click="writeDB(studio)">Save</button>
-
 </template>
 
 <script>
 
 import {Studio} from "@/classes/Studio";
+import IconButton from "@/components/kitchenSink/IconButton.vue";
+import BackgroundTile from "@/components/kitchenSink/BackgroundTile.vue";
+import CustomButton from "@/components/kitchenSink/CustomButton.vue";
 
 export default {
   name: "PersonEdit",
+  components: {CustomButton, BackgroundTile, IconButton},
 
   data() {
     return {
@@ -43,7 +56,7 @@ export default {
     async writeDB(studio) {
       window.ipcRenderer.removeAllListeners("gotStudios")
 
-      window.ipcRenderer.send('editDB', "UPDATE studio SET name = '" + this.name + "', popularity = '" + this.popularity+ "' WHERE pk_studioID = " + studio.id)
+      window.ipcRenderer.send('editDB', "UPDATE studio SET name = '" + this.name + "', popularity = '" + this.popularity + "' WHERE pk_studioID = " + studio.id)
 
       this.$store.state.otherStudios = []
       await new Promise(resolve => setTimeout(resolve, 200))
@@ -59,5 +72,44 @@ export default {
 </script>
 
 <style scoped>
+#studioEditMainDiv {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
 
+#dbEditorBackButton {
+  position: absolute;
+  float: left;
+  left: 100px;
+  top: 20px;
+}
+
+#studioEditBgTile {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  gap: 10px;
+  width: fit-content;
+}
+
+.studioEditInputLine {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  gap: 10px;
+}
+
+input {
+  background-color: var(--fsm-dark-blue-4);
+  border-radius: var(--fsm-s-border-radius);
+  border-style: none;
+  padding: 10px;
+}
+
+input:focus {
+  outline: none;
+}
 </style>
