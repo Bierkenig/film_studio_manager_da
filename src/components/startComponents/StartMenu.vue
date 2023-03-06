b1fe3570ba47d9db41777c25e267bf22eb77150f
Benedikt345 <92359939+Benedikt345@users.noreply.github.com>
2023-03-06 19:51:36 +0100

    update start menu

--- src/components/startComponents/StartMenu.vue
+++ src/components/startComponents/StartMenu.vue
@@ -51,9 +51,6 @@
                 </button>
               </router-link>
             </div>
-            <router-link :to="{ name: 'kitchenSink'}">
-              <button id="kitchenSinkButton" class="buttonStyle">kitchen-sink</button>
-            </router-link>
 
             <!--<router-link :to="{ name: 'SelectSlotWindow'}">
               <button id="slotButton" class="buttonStyle">Select Slot</button>
